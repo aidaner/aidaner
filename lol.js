aa752d99f1586c24be07e46b9d0653ecc8d3910e
Aidan <58609006+aidaner@users.noreply.github.com>
2021-10-08 21:19:19 -0400

    Update lol.js

--- lol.js
+++ lol.js
@@ -1,1 +1,6 @@
-var script = document.createElement('script'); script.src="//cdn.jsdelivr.net/npm/eruda"; document.body.appendChild(script); script.onload = function () { eruda.init() } }
+var script = document.createElement('script');
+script.src = "//cdn.jsdelivr.net/npm/eruda";
+document.body.appendChild(script);
+script.onload = function() {
+  eruda.init();
+};
